d5ccbd4631fd08e43d502bf8a0e5d948f4f59074
“Enter <anirudhanand984@gmail.com>
2021-12-16 14:35:07 +0530

    [Anirudh] add : add Terisa in both sales and marketing department

--- payroll_service.sql
+++ payroll_service.sql
@@ -3,7 +3,7 @@ create database payroll_service;
 use payroll_service;
 
 /*UC2*/
-create table employee_payrol(id int identity(1,1) primary key, name varchar(150) Not NULL, salary float NOT NULL, start date NOT NULL);
+create table employee_payroll(id int identity(1,1) primary key, name varchar(150) Not NULL, salary float NOT NULL, start date NOT NULL);
 
 /*UC3*/
 insert into employee_payroll values ('Bill',20000,'2021-12-13'), ('Charlie',15000,'2021-12-14'), ('Terisa',18000,'2021-12-14');
@@ -40,9 +40,10 @@ Alter table employee_payroll add deductions float , taxable_pay float, tax float
 /*UC10*/
 update employee_payroll set department ='Sales' where name='Terisa';
 insert into employee_payroll
-(name,salary,start,gender,phone_nmber,department,basic_pay,deductions,taxable_pay,tax,net_pay)values
-('Terisa',300000,'2021-12-12','F','7905142218','Marketing',300000,3000,25000,2000,2500000);
+(name,basic_pay,start,gender,phone_nmber,department,deductions,taxable_pay,tax,net_pay)values
+('Terisa',300000,'2021-12-12','F','7905142218','Marketing',3000,25000,2000,2500000);
 update employee_payroll set basic_pay=2500000, deductions=2500, taxable_pay=3000, net_pay=200000 where name='Terisa';
 select * from employee_payroll;
+
 
 
