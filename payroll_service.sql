7869c7a82ba2afe1973fe5c065718de781694132
Ilook18 <78365360+Ilook18@users.noreply.github.com>
2021-12-16 14:14:53 +0530

    [Anirudh] add : created table employee payroll

--- payroll_service.sql
+++ payroll_service.sql
@@ -3,7 +3,7 @@ create database payroll_service;
 use payroll_service;
 
 /*UC2*/
-create table employee_payrol(id int identity(1,1) primary key, name varchar(150) Not NULL, salary float NOT NULL, start date NOT NULL);
+create table employee_payroll(id int identity(1,1) primary key, name varchar(150) Not NULL, salary float NOT NULL, start date NOT NULL);
 
 
 
