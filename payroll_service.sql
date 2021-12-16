3f6fa9071ec4491ea5459a1f08219da88be3a349
Ilook18 <78365360+Ilook18@users.noreply.github.com>
2021-12-16 14:18:36 +0530

    [Anirudh] add : Select query to select the table

--- payroll_service.sql
+++ payroll_service.sql
@@ -3,7 +3,7 @@ create database payroll_service;
 use payroll_service;
 
 /*UC2*/
-create table employee_payrol(id int identity(1,1) primary key, name varchar(150) Not NULL, salary float NOT NULL, start date NOT NULL);
+create table employee_payroll(id int identity(1,1) primary key, name varchar(150) Not NULL, salary float NOT NULL, start date NOT NULL);
 
 /*UC3*/
 insert into employee_payroll values ('Bill',20000,'2021-12-13'), ('Charlie',15000,'2021-12-14'), ('Terisa',18000,'2021-12-14');
