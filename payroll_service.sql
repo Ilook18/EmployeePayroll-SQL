83b77657249c1c1d35904faf95daca08b22d6bcf
“Enter <anirudhanand984@gmail.com>
2021-12-16 08:02:51 +0530

    [Anirudh] add : inserted values into employee payroll table

--- payroll_service.sql
+++ payroll_service.sql
@@ -5,7 +5,8 @@ use payroll_service;
 /*UC2*/
 create table employee_payrol(id int identity(1,1) primary key, name varchar(150) Not NULL, salary float NOT NULL, start date NOT NULL);
 
-
+/*UC3*/
+insert into employee_payroll values ('Bill',20000,'2021-12-13'), ('Charlie',15000,'2021-12-14'), ('Terisa',18000,'2021-12-14');
 
 
 
