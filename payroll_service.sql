26a4e29f812b0eea8a6791accb7187340e7e591c
“Enter <anirudhanand984@gmail.com>
2021-12-16 07:48:04 +0530

    [Anirudh] add : created employee payroll table

--- payroll_service.sql
+++ payroll_service.sql
@@ -1,6 +1,8 @@
+/*UC1*/	
 create database payroll_service;
 use payroll_service;
 
-
+/*UC2*/
+create table employee_payrol(id int identity(1,1) primary key, name varchar(150) Not NULL, salary float NOT NULL, start date NOT NULL);
 
 
