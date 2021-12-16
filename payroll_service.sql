bc34acdba469b6440f11c2aa75388e8d8ce77463
“Enter <anirudhanand984@gmail.com>
2021-12-16 12:25:31 +0530

    [Anirudh] add : retrieve salary and date from the table

--- payroll_service.sql
+++ payroll_service.sql
@@ -11,5 +11,10 @@ insert into employee_payroll values ('Bill',20000,'2021-12-13'), ('Charlie',1500
 /*UC4*/
 select * from employee_payroll;
 
+/*UC5*/
+select name,salary from employee_payroll where name='Bill';
+select * from employee_payroll WHERE start BETWEEN CAST('2021-12-12' AS DATE) AND GETDATE();
+
+
 
 
