8a7ff42ac039ef40209e0b63642b6f83d0c38ddb
“Enter <anirudhanand984@gmail.com>
2021-12-16 08:33:25 +0530

    [Anirudh] add : Select query to select the table

--- payroll_service.sql
+++ payroll_service.sql
@@ -8,5 +8,8 @@ create table employee_payrol(id int identity(1,1) primary key, name varchar(150)
 /*UC3*/
 insert into employee_payroll values ('Bill',20000,'2021-12-13'), ('Charlie',15000,'2021-12-14'), ('Terisa',18000,'2021-12-14');
 
+/*UC4*/
+select * from employee_payroll;
+
 
 
